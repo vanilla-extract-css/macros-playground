2cb83b2c5a2ec225eb96c527635ef64065a7ffff
mattcompiles <mattjones701@gmail.com>
2023-07-24 17:15:44 +1000

    Update snapshot version

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vanillaExtractPlugin(), react(),],
+  plugins: [vanillaExtractPlugin(), react()],
 })
 
